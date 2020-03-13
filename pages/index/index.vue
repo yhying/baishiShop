749631fd13ee49120fd22dbd8085eef4c34884a9
18627805032@163.com <18627805032@163.com>
2020-03-13 13:16:42 +0800

    首页切换实现

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -9,28 +9,41 @@
 			<swiper-item v-for="(item,i) in newTopBar" :key="i">
 				<!-- <view class="home-nav"> -->
 				<!-- 推荐模板 -->
-				<scroll-view scroll-y="true" :style="'height:'+ClientHeight+'px;'">
-					<block v-for="(k,i1) in item.data" :key="i1">
-						<Swiper v-if="k.type=='swiperList'" :dataList="k.data"></Swiper>
-						<template v-if="k.type=='recommendList'">
-							<Recommend :dataList="k.data"></Recommend>
-							<Card cardTitle="猜你喜欢"></Card>
-						</template>
-						<GoodList v-if="k.type=='commodityList'" :dataList="k.data"></GoodList>
+				<scroll-view scroll-y="true" :style="'height:'+ClientHeight+'px;'" @scrolltolower="loadmore(i)">
+					<block v-if="item.data.length>0">
+						<block v-for="(k,i1) in item.data" :key="i1">
+							<Swiper v-if="k.type=='swiperList'" :dataList="k.data"></Swiper>
+							<template v-if="k.type=='recommendList'">
+								<Recommend :dataList="k.data"></Recommend>
+								<Card cardTitle="猜你喜欢"></Card>
+							</template>
+							<!-- 其他模板 -->
+							<Banner v-if="k.type=='bannerList'" :dataList="k.imgUrl"></Banner>
+							<template v-if="k.type=='iconsList'">
+								<Icons :dataList="k.data"></Icons>
+								<Card cardTitle="热销爆品"></Card>
+							</template>
+							<template v-if="k.type=='iconsList'">
+								<Hots :dataList="k.data"></Hots>
+								<Card cardTitle="推荐店铺"></Card>
+							</template>
+							<template v-if="k.type=='shopList'">
+								<Shop :dataList="k.data"></Shop>
+								<Card cardTitle="为您推荐"></Card>
+							</template>
+							<GoodList v-if="k.type=='commodityList'" :dataList="k.data"></GoodList>
+						</block>
 					</block>
+					<view v-else>
+						暂无数据...
+					</view>
+					<view class="loadText f-color">
+						{{item.loadText}}
+					</view>
 				</scroll-view>
 				<!-- </view> -->
 			</swiper-item>
 		</swiper>
-		<!-- 其他模板 -->
-		<!-- 				<Banner></Banner>
-		<Icons></Icons>
-		<Card cardTitle="热销爆品"></Card>
-		<Hots></Hots>
-		<Card cardTitle="推荐店铺"></Card>
-		<Shop></Shop>
-		<Card cardTitle="为您推荐"></Card>
-		<GoodList></GoodList> -->
 	</view>
 </template>
 
@@ -58,7 +71,7 @@
 			return {
 				tabBarIndex: 0,
 				ScrollIndex: 'top0',
-				ClientHeight:0,
+				ClientHeight: 0,
 				topBar: [],
 				// 承载数据
 				newTopBar: []
@@ -77,7 +90,7 @@
 			// 获取系统高度
 			uni.getSystemInfo({
 				success: (res) => {
-					this.ClientHeight=res.windowHeight-uni.upx2px(80)
+					this.ClientHeight = res.windowHeight - uni.upx2px(80) - this.getClientHeight()
 				}
 			})
 		},
@@ -86,30 +99,21 @@
 			tabBarClick(index) {
 				this.tabBarIndex = index
 				this.ScrollIndex = 'top' + index
+				if (this.newTopBar[index].loadding == 'first') {
+					this.getindexOtherData()
+				}
 			},
 			// 监听内容滑块切换事件
 			SwiperChange(e) {
-				this.tabBarIndex = e.detail.current
-				this.ScrollIndex = 'top' + e.detail.current
-			},
-			// 请求首页数据
-			getindexData() {
-				uni.request({
-					url: "http://192.168.1.10:3000/api/index/data",
-					success: (res) => {
-						console.log(res)
-						let data = res.data.data;
-						this.topBar = data.topBar;
-						this.newTopBar = this.initData(data);
-						console.log(this.newTopBar)
-					}
-				})
+				this.tabBarClick(e.detail.current);
 			},
 			initData(res) {
 				let arr = []
 				this.topBar.forEach((item, i) => {
 					let obj = {
-						data: []
+						data: [],
+						loadding: 'first',
+						loadText: '上拉加载更多...'
 					}
 					if (i == 0) {
 						obj.data = res.data
@@ -118,6 +122,70 @@
 					arr.push(obj)
 				})
 				return arr
+			},
+			// 监听上拉加载
+			loadmore(index) {
+				this.newTopBar[index].loadText = '加载中...';
+				this.getindexOtherData(() => {
+					this.newTopBar[index].loadText = '上拉加载更多...';
+				})
+			},
+			//监听客户端，兼容
+			getClientHeight() {
+				const res = uni.getSystemInfoSync();
+				const system = res.platform;
+				console.log(system)
+				if (system === 'ios') {
+					// return 44+res.statusBarHeight;
+					return 0;
+				} else if (system === 'android') {
+					// return 48+res.statusBarHeight;
+					return 0;
+				} else {
+					return 0;
+				}
+			},
+			/* 
+			网络请求事件监听
+			 */
+			// 请求首页数据
+			getindexData() {
+				uni.request({
+					url: "http://192.168.1.10:3000/api/index/data",
+					success: (res) => {
+						// console.log(res)
+						let data = res.data.data;
+						this.topBar = data.topBar;
+						this.newTopBar = this.initData(data);
+						// console.log(this.newTopBar)
+					}
+				})
+			},
+			// 其他模板首页数据
+			getindexOtherData(callback) {
+				let index = this.tabBarIndex
+				let topBarid = this.topBar[index].id
+				console.log(this.newTopBar)
+				let pageId = Math.ceil(this.newTopBar[index].data.length / 5) + 1; 
+				uni.request({
+					// url: "http://192.168.1.10:3000/api/index/"+ topBarid +"/data/"+pageId, 
+					url: `http://192.168.1.10:3000/api/index/${topBarid}/data/${pageId}`,
+					/* ES6*/
+					success: (res) => {
+						console.log(res.statusCode)
+						if (res.statusCode != 200) {
+							return;
+						} else {
+							let data = res.data.data;
+							this.newTopBar[index].data = [...this.newTopBar[index].data, ...data]
+						}
+					},
+				})
+				// 请求结束后，关上loadding
+				this.newTopBar[index].loadding = 'last'
+				if(typeof callback==='function'){
+					callback();
+				}
 			}
 		}
 	}
@@ -143,5 +211,11 @@
 	.f-active-color {
 		padding: 10rpx 0;
 		border-bottom: 6rpx solid #49BDFB;
+	}
+
+	.loadText {
+		border-top: 2rpx solid #636263;
+		line-height: 60rpx;
+		text-align: center;
 	}
 </style>
