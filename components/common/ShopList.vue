1bbc406a2a63df5a7a7b73e644907a9481de2689
18627805032@163.com <18627805032@163.com>
2020-03-15 15:29:35 +0800

    搜索记录持久化

--- components/common/ShopList.vue
+++ components/common/ShopList.vue
@@ -1,45 +1,95 @@
 <template>
 	<view class="ShopList f-color">
 		<view class="shop-title">
-			<view class="shop-item">
-				<view>价格</view>
+			<view class="shop-item" v-for="(item,index) in Shopbar.data" :key="index" @tap="changeTab(index)">
+				<view :class="Shopbar.currentindex==index?'f-active-color':' '">{{item.name}}</view>
 				<view class="icons">
-					<view class="iconfont icon-shangjiantou up"></view>
-					<view class="iconfont icon-xiajiantou down"></view>
-				</view>
-			</view>
-			<view class="shop-item">
-				<view>折扣</view>
-				<view class="icons">
-					<view class="iconfont icon-shangjiantou up"></view>
-					<view class="iconfont icon-xiajiantou down"></view>
-				</view>
-			</view>
-			<view class="shop-item">
-				<view>品牌</view>
-				<view class="icons">
-					<view class="iconfont icon-shangjiantou up"></view>
-					<view class="iconfont icon-xiajiantou down"></view>
+					<view class="iconfont icon-shangjiantou up" :class="item.status==1?'f-active-color':''"></view>
+					<view class="iconfont icon-xiajiantou down" :class="item.status==2?'f-active-color':''"></view>
 				</view>
 			</view>
 		</view>
 		<Lines />
+		<goodList :dataList="dataList"></goodList>
 	</view>
 </template>
 
 <script>
 	import Lines from '@/components/common/Line.vue'
+	import goodList from '@/components/common/GoodList.vue'
 	export default {
 		components: {
-			Lines
+			Lines,
+			goodList
 		},
 		data() {
 			return {
-
+				Shopbar: {
+					currentindex: 0,
+					data: [{
+							name: '价格',
+							status: 1
+						},
+						{
+							name: '折扣',
+							status: 0
+						},
+						{
+							name: '品牌',
+							status: 0
+						}
+					]
+				},
+				dataList: [{
+						id: 1,
+						imgUrl: "../../static/img/commodity1.jpg",
+						name: "大姨绒毛大款2020年必须买,不买你就不行了,爆款疯狂GG008大姨绒毛大款2020年必须买,不买你就不行了,爆款疯狂GG008",
+						pprice: "299",
+						oprice: "659",
+						discount: "5.2"
+					},
+					{
+						id: 2,
+						imgUrl: "../../static/img/commodity2.jpg",
+						name: "大姨绒毛大款2020年必须买,不买你就不行了,爆款疯狂GG008大姨绒毛大款2020年必须买,不买你就不行了,爆款疯狂GG008",
+						pprice: "299",
+						oprice: "659",
+						discount: "5.2"
+					},
+					{
+						id: 3,
+						imgUrl: "../../static/img/commodity3.jpg",
+						name: "大姨绒毛大款2020年必须买,不买你就不行了,爆款疯狂GG008大姨绒毛大款2020年必须买,不买你就不行了,爆款疯狂GG008",
+						pprice: "299",
+						oprice: "659",
+						discount: "5.2"
+					},
+					{
+						id: 4,
+						imgUrl: "../../static/img/commodity4.jpg",
+						name: "大姨绒毛大款2020年必须买,不买你就不行了,爆款疯狂GG008大姨绒毛大款2020年必须买,不买你就不行了,爆款疯狂GG008",
+						pprice: "299",
+						oprice: "659",
+						discount: "5.2"
+					}
+				]
 			}
 		},
 		methods: {
+			changeTab(index) {
+				let idx = this.Shopbar.currentindex
+				//具体哪一个对象
+				let item = this.Shopbar.data[idx];
+				if (idx == index) {
+					return item.status = item.status === 1 ? 2 : 1;
+				}
+				//新的值
+				let newItem = this.Shopbar.data[index];
+				item.status = 0;
+				this.Shopbar.currentindex = index
+				newItem.status = 1;
 
+			}
 		}
 	}
 </script>
@@ -66,16 +116,20 @@
 		margin-left: 5rpx;
 		text-align: center;
 	}
+
 	.iconfont {
 		width: 16rpx;
-		height: 8rpx;;
+		height: 8rpx;
+		;
 		position: absolute;
 		left: 0;
 	}
+
 	.up {
-		top:-22rpx;
+		top: -22rpx;
 	}
+
 	.down {
-		top:-8rpx
+		top: -8rpx
 	}
 </style>
--- pages/search/search.vue
+++ pages/search/search.vue
@@ -6,10 +6,10 @@
 				<view class='f-color'>最近搜索</view>
 				<view class="iconfont icon-lajitong"></view>
 			</view>
-			<view>
-				<view class="search-name">四件套</view>
-				<view class="search-name">面膜</view>
+			<view v-if='history.length > 0'>
+				<view class="search-name" v-for="(item,index) in history" :key="index">{{item}}</view>
 			</view>
+			<view class="search-end" v-else>暂无搜索记录</view>
 		</view>
 		<view class="search-item f-color">
 			<view class='search-title'>
@@ -31,19 +31,63 @@
 		},
 		data() {
 			return {
-
+				// 搜索关键字
+				keyWord: '',
+				history: []
 			}
 		},
-		onNavigationBarButtonTap(e){
-			console.log(e)
-			if(e.float=='right'){
-				uni.navigateTo({
-				    url: '../search-List/search-List'
-				})
-			}
+		onShow() {
+			uni.getStorage({
+				key: 'history_key',
+				success: res => {
+					this.history = res.data
+				}
+			});
+		},
+		//点击顶栏中的搜索按钮
+		onNavigationBarButtonTap(e) {
+			this.ToSearch()
+		},
+		//监听软键盘的搜索按钮点击的
+		onNavigationBarSearchInputConfirmed() {
+			this.search();
+		},
+		//监听input输入内容
+		onNavigationBarSearchInputChanged(e) {
+			// console.log(e)
+			this.keyWord = e.text
 		},
 		methods: {
-
+			ToSearch() {
+				if (this.keyWord === '') {
+					uni.showToast({
+						title: '关键词不能为空',
+						icon: 'none'
+					})
+				} else {
+					uni.navigateTo({
+						url: '../search-List/search-List',
+						success: () => {
+							this.addSearch()
+						}
+					})
+				}
+				// 隐藏键盘
+				uni.hideKeyboard();
+			},
+			addSearch() {
+				let idx=this.history.indexOf(this.keyWord)
+				if(idx==-1){
+					this.history.unshift(this.keyWord)
+				}else{
+					this.history.splice(idx,1)            /* 删除原有的记录*/
+					this.history.unshift(this.keyWord)    /* 重新追加到前面*/
+				}
+				uni.setStorage({
+					key: 'history_key',
+					data: this.history
+				});
+			}
 		}
 	}
 </script>
@@ -53,15 +97,20 @@
 		padding: 20rpx;
 		font-size: 28rpx;
 	}
+
 	.search-title {
 		display: flex;
 		justify-content: space-between;
 	}
+
 	.search-name {
 		display: inline-block;
 		padding: 4rpx 24rpx;
 		border-radius: 26rpx;
 		background-color: #e1e1e1;
 		margin: 10rpx;
+	}
+	.search-end {
+		text-align: center;
 	}
 </style>
