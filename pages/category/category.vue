a4a3224d83e0a41bbc06d48180ca8a335dc94eb2
18627805032@163.com <18627805032@163.com>
2020-03-17 16:07:59 +0800

    分类页面

--- pages/category/category.vue
+++ pages/category/category.vue
@@ -1,24 +1,149 @@
 <template>
 	<view class="content">
-		<view class="f-active-color">123</view>
-		<view class="iconfont icon-xiaoxi"></view>
+		<Lines />
+		<view class="list">
+			<!-- 左侧 -->
+			<scroll-view scroll-y="true" class='list-left' :style="'height:'+clentHeight+'px'">
+				<view v-for='(item,index) in goods' :key="index" class='left-item'  @tap="changeLeftTab(index)">
+					<view class='left-name' :class="activeIndex===index?'left-name-active':''">{{item.name}}</view>
+				</view>
+			</scroll-view>
+			<!-- 右侧 -->
+			<scroll-view scroll-y="true" class='list-right' :style="'height:'+clentHeight+'px'">
+				<view class="right-list" v-for="(item,index) in goods" :key="index">
+					<block v-for="(k,i) in item.data" :key="i">
+						<view class="list-title">{{k.name}}</view>
+						<view class="right-content">
+							<view class="right-item" v-for='(k2,i2) in k.list' :key="i2"> 
+								<image :src="k2.imgUrl" mode=""></image>
+								<view class="right-name">{{k2.name}}</view>
+							</view>
+						</view>
+					</block>
+				</view>
+			</scroll-view>
+		</view>
 	</view>
 </template>
 
 <script>
+	import Lines from '@/components/common/Line.vue'
+		import $http from '@/common/api/request.js'
 	export default {
+		components:{
+			Lines
+		},
 		data() {
 			return {
+				clentHeight:'',
+				activeIndex:0,
+				goods:[]
 			}
 		},
 		onLoad() {
-
+			this.getgoodsData()
+		},
+		//获取可视高度
+		onReady() {
+			uni.getSystemInfo({
+				success: (res) => {
+					this.clentHeight = res.windowHeight - this.getClientHeight();
+				}
+			})
+		},
+		// 点击搜索栏跳转搜索页面
+		onNavigationBarSearchInputClicked(){
+			console.log(111)
+			uni.navigateTo({
+			    url: '../search/search'
+			})
 		},
 		methods: {
-
+			// 点击左侧切换兰
+			changeLeftTab(index){
+				this.activeIndex=index
+			},
+			//监听客户端，兼容
+			getClientHeight() {
+				const res = uni.getSystemInfoSync();
+				const system = res.platform;
+				console.log(system)
+				if (system === 'ios') {
+					// return 44+res.statusBarHeight;
+					return 0;
+				} else if (system === 'android') {
+					// return 48+res.statusBarHeight;
+					return 0;
+				} else {
+					return 0;
+				}
+			},
+			// 获取网络请求数据
+			getgoodsData(){
+				$http.request({
+					url:'/goods/list'
+				}).then(res=>{
+					console.log(res)
+					this.goods=res
+				}).catch(()=>{
+					uni.showToast({
+						title: '请求失败',
+						icon: 'none'
+					})
+				})
+			}
 		}
 	}
 </script>
 
-<style>
+<style scoped>
+	.list {
+		display: flex;
+	}
+	.list-left {
+		width: 200rpx;
+	}
+	.left-item {
+		border-bottom: 2rpx solid #fff;
+		font-size: 28rpx;
+		font-weight: 700;
+		background-color: #f7f7f7;
+	}
+	.left-name {
+		padding:30rpx 6rpx;
+		text-align: center;
+	}
+	.left-name-active{
+		border-left:8rpx solid #49BDFB;
+		background-color: #FFFFFF;
+	}
+.list-right{
+	flex:1;
+	padding-left:30rpx;
+}
+.list-title{
+	font-weight: bold;
+	padding:30rpx 0;
+}
+.right-content {
+	display: flex;
+	flex-wrap: wrap;
+	/* justify-content: center; */
+}
+.right-item{
+	width: 150rpx;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	padding: 10rpx;
+	
+}
+.right-name{
+	padding:16rpx 0;
+}
+.right-item image {
+	width: 150rpx;
+	height: 150rpx;
+}
 </style>
